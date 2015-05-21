93292a2dd3e68df8cafa705a7cfabb91ac7f9768
angelamerino <angelamerino9@gmail.com>
2015-05-21 15:26:51 -0600

    Se actualizó plantilla con menú y banner y se crea pagina para catalogos y manejador

---
+++ src/main/java/sv/gob/cultura/rrhh/manejadores/manejadorCatalogos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.gob.cultura.rrhh.manejadores;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author Angela
+ */
+@Named(value = "manejadorCatalogos")
+@Dependent
+public class manejadorCatalogos {
+
+    /**
+     * Creates a new instance of manejadorCatalogos
+     */
+    public manejadorCatalogos() {
+    }
+    
+}
